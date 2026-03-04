a938374f2c3ce9761bf56b1eedb4809eee410b5b
HALIMATUMATANA <halimatumatana@gmail.com>
2026-03-04 04:17:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1627542812-fdf9c",
+  "appId": "1:843828100307:web:9f0167a1f6d1ad5665a826",
+  "apiKey": "AIzaSyCr7TkwB_omVlCleteaPmHBQGvUK7Esafo",
+  "authDomain": "studio-1627542812-fdf9c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "843828100307"
+};
